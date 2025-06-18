31c6464cfdd907dec36b9efefa523eedc7e732dd
hemanth <hemanth02135@gmail.com>
2025-06-18 13:36:04 +0530

    projects

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -25,11 +25,53 @@ export const projects = [
   },
 
   {
-    name: "Imagely",
-    description: "Admin dashboard with product, order, and user management.",
+    name: "PhotoGenAi",
+    slug: "PhotoGenAi",
+    image: {
+      url: "/image.png",
+      width: 800,
+      height: 200,
+      alt: "image gen",
+    },
+    urls: {
+      githubUrl: "https://github.com/hemanth-1321/Ai-img-gen",
+    },
+    description:
+      "A powerful Al-powered image generation application where users can upload their images, train a model, and use it to generate their own images. Built using the FluxLoRA Al model, it provides a customizable image-generation experience",
+    tags: [
+      "Next.js",
+      "TypeScript",
+      "Prisma",
+      "Shadcn UI",
+      "Tailwind CSS",
+      "Aceternity UI",
+      "Node js",
+    ],
   },
+
   {
-    name: "photoAi",
-    description: "Real-time messaging app with Socket.io and MongoDB.",
+    name: "Manimator",
+    slug: "Manimator",
+    image: {
+      url: "/manim.png",
+      width: 800,
+      height: 200,
+      alt: "video gen",
+    },
+    urls: {
+      githubUrl: "https://github.com/hemanth-1321/manim-videos.git",
+    },
+    description:
+      "A platform where users can input plain text to generate 2D animations using the Python Manim library. While it is not yet suitable for complex mathematical animations, it works well for simple animations. The platform is built using FastAPI for the backend and Next.js for the frontend",
+    tags: [
+      "Next.js",
+      "TypeScript",
+      "Prisma",
+      "Shadcn UI",
+      "Tailwind CSS",
+      "Aceternity UI",
+      "python",
+      "fast API",
+    ],
   },
 ];
